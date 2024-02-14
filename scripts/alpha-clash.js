ef4fabb0fccde61db3447490926d33aa0898ebf2
gmhridu <hasanmahabubhridoy9@gmail.com>
2024-02-14 12:30:45 +0600

    added life score

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,26 @@
+// function play() {
+//   // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//   const homeSection = document.getElementById('home-screen');
+//   homeSection.classList.add('hidden');
+//   // show the playground
+//   const playGroundSection = document.getElementById("play-ground");
+//   playGroundSection.classList.remove('hidden');
+// }
+function continueGame() {
+  // generate a random alphabet
+  const alphabet = getARandomAlphabet();
+  console.log("your random alphabet", alphabet);
+
+  // set randomly generated alphabet to the screen (show it)
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  currentAlphabetElement.innerText = alphabet;
+
+  // set background color
+  setBackgroundColorById(alphabet);
+}
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  continueGame();
+}
